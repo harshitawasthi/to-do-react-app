1e0548a0c291f7df9c2ae28268fe1e579267282c
harshit <harshitawasthi.com>
2021-04-06 12:23:37 +0530

    Updates

---
+++ precache-manifest.886928f841e073feb1036a6643d39070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fd89082ef3b624dae5557bd9a3b345",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe4ad4f0990f322cada5",
+    "url": "./static/css/main.bf7b55e4.chunk.css"
+  },
+  {
+    "revision": "8a8605308c60690694a1",
+    "url": "./static/js/2.2c4f6304.chunk.js"
+  },
+  {
+    "revision": "fe4ad4f0990f322cada5",
+    "url": "./static/js/main.00e221c0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
